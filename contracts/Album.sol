c98190380c86b71b0d79f48fd1ee0445e9b6f740
Pritam Nikam <pritam.nikam@gmail.com>
2022-06-17 12:40:05 -0600

    Update Album.sol

--- contracts/Album.sol
+++ contracts/Album.sol
@@ -2,9 +2,11 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "./Utility.sol";
+
 // A smart contract to model the Album
 
-contract Album {
+contract Album is Utility {
 
      // A custom data structure used to define a music album
     struct musicAlbum {
@@ -23,31 +25,11 @@ contract Album {
     // A mapping of every user's favorite album
     mapping(address => musicAlbum) public userAlbums;
 
-
     // the author of the smart contract
     string public constant contractAuthor = "Pritam Nikam";
 
-    // The owner of the current instance of this smart contract
-    address owner;
-
-    // Event which will be raised anytime the current album information is updated.
-    event errorEvent(string errorEvent_Description);
-
     // Event which will be raised anytime the current album information is updated.
     event albumEvent(string albumEvent_Description, string albumEvent_Artist, string albumEvent_Title, uint albumEvent_Tracks);
-
-    // This function modifier ensures that the initiator of any transaction 
-    //   it is attached to matches the address of the contract's owner.
-    // Use this function modifier for functions that should only
-    //   be performed by the owner of this contract instance.
-    modifier onlyOwner {
-        if (msg.sender != owner) {
-            // The initiator of this transaction is NOT the contract instance's owner!
-            emit errorEvent("Only the owner of this contract instance can perform this function!");
-        } else {
-            _;
-        } // else
-    } // modifier onlyOwner
 
     // Contract constructor.
     //   This code is called once when the contract instance is deployed to the Ethereum network
